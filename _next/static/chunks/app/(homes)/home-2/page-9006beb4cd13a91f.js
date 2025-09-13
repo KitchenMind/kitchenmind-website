b5c19ec4f6320273d755606ef0dffb1debd29060
thepKz <thepKz@users.noreply.github.com>
2025-09-13 04:20:33 +0000

    deploy: e0d1ea855afc9ffabbb2682bdc2086684da4a398

---
+++ _next/static/chunks/app/(homes)/home-2/page-9006beb4cd13a91f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[937],{2925:function(n,e,u){Promise.resolve().then(u.bind(u,3889))}},function(n){n.O(0,[8038,922,5583,8536,7160,2249,5652,3889,2971,7023,1744],function(){return n(n.s=2925)}),_N_E=n.O()}]);
